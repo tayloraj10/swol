427cbaae8fe0324eb1d7804672201fd55bb7ede4
tayloraj10 <taylor.alan.johnson@gmail.com>
2024-11-10 22:04:54 -0500

    add scrollbar

--- lib/components/current_workouts.dart
+++ lib/components/current_workouts.dart
@@ -45,67 +45,69 @@ class _CurrentWorkoutsState extends State<CurrentWorkouts> {
           );
         } else {
           final data = snapshot.data!;
-          return ListView.builder(
-            shrinkWrap: true,
-            itemCount: data.docs.length,
-            itemBuilder: (context, index) {
-              final exercise = data.docs[index];
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                child: Column(
-                  children: [
-                    SingleWorkout(
-                      exercise: exercise,
-                      lastWorkout: index == data.docs.length - 1,
-                    ),
-                    if (widget.showPastExercises)
-                      StreamBuilder<QuerySnapshot>(
-                        key: UniqueKey(),
-                        stream: FirebaseFirestore.instance
-                            .collection('workouts_calisthenics')
-                            .where('exercise_id',
-                                isEqualTo: exercise['exercise_id'])
-                            .orderBy('date', descending: true)
-                            .snapshots(),
-                        builder:
-                            (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(
-                                child: CircularProgressIndicator());
-                          } else if (snapshot.hasError) {
-                            return Center(
-                                child: Text('Error: ${snapshot.error}'));
-                          }
-                          // else if (!snapshot.hasData ||
-                          //     snapshot.data!.docs.length <= 1) {
-                          //   return const Text('No Previous Exercises');
-                          // }
-                          else {
-                            final data = snapshot.data!;
-                            return Column(
-                              children: [
-                                ListView.builder(
-                                  shrinkWrap: true,
-                                  itemCount: data.docs.length - 1,
-                                  itemBuilder: (context, index) {
-                                    final oldExercise = data.docs[index + 1];
-                                    return SingleWorkout(
-                                      exercise: oldExercise,
-                                      pastExercise: true,
-                                    );
-                                  },
-                                ),
-                              ],
-                            );
-                          }
-                        },
+          return Scrollbar(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: data.docs.length,
+              itemBuilder: (context, index) {
+                final exercise = data.docs[index];
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                  child: Column(
+                    children: [
+                      SingleWorkout(
+                        exercise: exercise,
+                        lastWorkout: index == data.docs.length - 1,
                       ),
-                  ],
-                ),
-              );
-            },
+                      if (widget.showPastExercises)
+                        StreamBuilder<QuerySnapshot>(
+                          key: UniqueKey(),
+                          stream: FirebaseFirestore.instance
+                              .collection('workouts_calisthenics')
+                              .where('exercise_id',
+                                  isEqualTo: exercise['exercise_id'])
+                              .orderBy('date', descending: true)
+                              .snapshots(),
+                          builder:
+                              (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            } else if (snapshot.hasError) {
+                              return Center(
+                                  child: Text('Error: ${snapshot.error}'));
+                            }
+                            // else if (!snapshot.hasData ||
+                            //     snapshot.data!.docs.length <= 1) {
+                            //   return const Text('No Previous Exercises');
+                            // }
+                            else {
+                              final data = snapshot.data!;
+                              return Column(
+                                children: [
+                                  ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: data.docs.length - 1,
+                                    itemBuilder: (context, index) {
+                                      final oldExercise = data.docs[index + 1];
+                                      return SingleWorkout(
+                                        exercise: oldExercise,
+                                        pastExercise: true,
+                                      );
+                                    },
+                                  ),
+                                ],
+                              );
+                            }
+                          },
+                        ),
+                    ],
+                  ),
+                );
+              },
+            ),
           );
         }
       },
