8474f5d73cdfdcd19d013d9ad7a652922219b7e4
tayloraj10 <taylor.alan.johnson@gmail.com>
2025-03-02 01:10:35 -0500

    show running list of exercises instead of day by day

--- lib/components/current_workouts.dart
+++ lib/components/current_workouts.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:swole/components/single_workout.dart';
 
 class CurrentWorkouts extends StatefulWidget {
@@ -29,12 +30,9 @@ class _CurrentWorkoutsState extends State<CurrentWorkouts> {
           .collection('workouts_calisthenics')
           .where('date',
               isGreaterThanOrEqualTo: Timestamp.fromDate(DateTime(
-                  widget.date.year, widget.date.month, widget.date.day)))
-          .where('date',
-              isLessThan: Timestamp.fromDate(
-                  DateTime(widget.date.year, widget.date.month, widget.date.day)
-                      .add(const Duration(days: 1))))
+                  widget.date.year, widget.date.month - 1, widget.date.day)))
           .where('user_id', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .orderBy('date', descending: true)
           .snapshots(),
       builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -43,7 +41,7 @@ class _CurrentWorkoutsState extends State<CurrentWorkouts> {
             child: Center(child: CircularProgressIndicator()),
           );
         } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
+          return Center(child: SelectableText('Error: ${snapshot.error}'));
         } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
           return const Padding(
             padding: EdgeInsets.all(20),
@@ -51,75 +49,103 @@ class _CurrentWorkoutsState extends State<CurrentWorkouts> {
           );
         } else {
           final data = snapshot.data!;
+          Map<String, List<QueryDocumentSnapshot>> groupedData = {};
+
+          for (var doc in data.docs) {
+            String date =
+                DateFormat('M/dd').format((doc['date'] as Timestamp).toDate());
+            if (groupedData[date] == null) {
+              groupedData[date] = [];
+            }
+            groupedData[date]!.add(doc);
+          }
+
           return Scrollbar(
             child: ListView.builder(
               shrinkWrap: true,
-              itemCount: data.docs.length,
+              itemCount: groupedData.keys.length,
               itemBuilder: (context, index) {
-                final exercise = data.docs[index];
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                  child: Column(
-                    children: [
-                      SingleWorkout(
-                        exercise: exercise,
-                        lastWorkout: index == data.docs.length - 1,
-                        previousTotal: previousTotal,
+                String date = groupedData.keys.elementAt(index);
+                List<QueryDocumentSnapshot> exercises = groupedData[date]!;
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 20),
+                      child: Text(
+                        date,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
-                      if (widget.showPastExercises)
-                        StreamBuilder<QuerySnapshot>(
-                          key: UniqueKey(),
-                          stream: FirebaseFirestore.instance
-                              .collection('workouts_calisthenics')
-                              .where('exercise_id',
-                                  isEqualTo: exercise['exercise_id'])
-                              .orderBy('date', descending: true)
-                              .limit(4)
-                              .snapshots(),
-                          builder:
-                              (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return const Center(
-                                  child: CircularProgressIndicator());
-                            } else if (snapshot.hasError) {
-                              return Center(
-                                  child: Text('Error: ${snapshot.error}'));
-                            }
-                            // else if (!snapshot.hasData ||
-                            //     snapshot.data!.docs.length <= 1) {
-                            //   return const Text('No Previous Exercises');
-                            // }
-                            else {
-                              final data = snapshot.data!;
-                              return Column(
-                                children: [
-                                  ListView.builder(
-                                    shrinkWrap: true,
-                                    itemCount: data.docs.length - 1,
-                                    itemBuilder: (context, index) {
-                                      final oldExercise = data.docs[index + 1];
-                                      if (index == 0) {
-                                        int totalReps = 0;
-                                        for (var set in oldExercise['sets']) {
-                                          totalReps += (set['reps'] as int);
-                                        }
-                                        updatePreviousTotal(totalReps);
-                                      }
-                                      return SingleWorkout(
-                                        exercise: oldExercise,
-                                        pastExercise: true,
-                                      );
-                                    },
-                                  ),
-                                ],
-                              );
-                            }
-                          },
+                    ),
+                    ...exercises.map((exercise) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 12, horizontal: 20),
+                        child: Column(
+                          children: [
+                            SingleWorkout(
+                              exercise: exercise,
+                              lastWorkout: false,
+                              previousTotal: previousTotal,
+                            ),
+                            if (widget.showPastExercises)
+                              StreamBuilder<QuerySnapshot>(
+                                key: UniqueKey(),
+                                stream: FirebaseFirestore.instance
+                                    .collection('workouts_calisthenics')
+                                    .where('exercise_id',
+                                        isEqualTo: exercise['exercise_id'])
+                                    .orderBy('date', descending: true)
+                                    .limit(4)
+                                    .snapshots(),
+                                builder: (context,
+                                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                                  if (snapshot.connectionState ==
+                                      ConnectionState.waiting) {
+                                    return const Center(
+                                        child: CircularProgressIndicator());
+                                  } else if (snapshot.hasError) {
+                                    return Center(
+                                        child:
+                                            Text('Error: ${snapshot.error}'));
+                                  } else {
+                                    final data = snapshot.data!;
+                                    return Column(
+                                      children: [
+                                        ListView.builder(
+                                          shrinkWrap: true,
+                                          itemCount: data.docs.length - 1,
+                                          itemBuilder: (context, index) {
+                                            final oldExercise =
+                                                data.docs[index + 1];
+                                            if (index == 0) {
+                                              int totalReps = 0;
+                                              for (var set
+                                                  in oldExercise['sets']) {
+                                                totalReps +=
+                                                    (set['reps'] as int);
+                                              }
+                                              updatePreviousTotal(totalReps);
+                                            }
+                                            return SingleWorkout(
+                                              exercise: oldExercise,
+                                              pastExercise: true,
+                                            );
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  }
+                                },
+                              ),
+                          ],
                         ),
-                    ],
-                  ),
+                      );
+                    }).toList(),
+                  ],
                 );
               },
             ),
