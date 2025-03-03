32a28e1fb5a61e5bd41ba053cba6b922bb4de424
tayloraj10 <taylor.alan.johnson@gmail.com>
2025-03-02 19:07:22 -0500

    add test user sign in

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,12 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:swole/components/nav_bar.dart';
 import 'package:swole/firebase_options.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +30,53 @@ class Login extends StatelessWidget {
             Navigator.pushNamed(context, '/home');
           }),
         ],
+        footerBuilder: (context, action) => Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: CupertinoButton.filled(
+            padding: EdgeInsets.zero,
+            borderRadius: BorderRadius.circular(6),
+            child: SizedBox(
+              height: 42,
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(6),
+                      bottomLeft: Radius.circular(6),
+                    ),
+                    child: Container(
+                      color: Colors.white,
+                      child: const SizedBox(
+                        width: 44,
+                        height: 42,
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Expanded(
+                      child: Text(
+                    'Sign in as test user',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  )),
+                ],
+              ),
+            ),
+            onPressed: () async {
+              if (!mounted) return;
+              await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: "test@gmail.com", password: "123456");
+              if (mounted) {
+                Navigator.pushNamed(context, '/home');
+              }
+            },
+          ),
+        ),
       ),
     );
   }
